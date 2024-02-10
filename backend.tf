95831175123c5a5437727f2715ab55bef4625fe7
venkatkukatla <104503378+venkatkukatla@users.noreply.github.com>
2024-02-10 08:21:06 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
-#terraform {
-  #backend "s3" {
-    #encrypt = true    
-    #bucket = "mybbhufgyyhhfhgy"
-    #dynamodb_table = "terraform-state-lock-dynamo"
-    #key    = "terraform.tfstate"
-    #region = "us-east-1"
+terraform {
+  backend "s3" {
+    encrypt = true    
+    bucket = "mybbhufgyyhhfhgy"
+    dynamodb_table = "terraform-state-lock-dynamo"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
   }
 }
